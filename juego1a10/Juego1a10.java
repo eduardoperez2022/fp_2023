0153ca6a82b630c036fa742a411a0a37ea3cd1b4
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2023-05-03 17:01:43 -0300

    Demo de juego numero aleatorio

---
+++ juego1a10/Juego1a10.java
@@ -0,0 +1,19 @@
+package juego1a10;
+
+/**
+ *
+ * @author eduardo
+ */
+public class Juego1a10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        juego jue = new juego();
+        jue.menu();
+        
+    }
+    
+}
---
+++ juego1a10/juego.java
@@ -0,0 +1,50 @@
+package juego1a10;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author eduardo
+ */
+public class juego {
+    
+    public int incognita;
+    
+    public void menu() {
+        
+        int numero=0;
+        
+        Scanner menu = new Scanner(System.in);
+        
+        this.calculo_incognita();
+        System.out.println(this.incognita);
+        System.out.println("Adivine un numero entre 0 y 9 !!! tiene tres intentos :");
+        // tres intentos con for
+        for(int i=0; i<3; i++) {
+            
+            numero = menu.nextInt();
+            
+            if (this.proceso_entrada(numero) == true) {
+                System.out.println("Adivino !!!");
+            }
+            else {
+                System.out.println("Casi ... intente de nuevo ...");
+            }
+        }
+    }
+    
+    public void calculo_incognita() {
+        this.incognita = (int)(Math.random()*10+1);   
+    }
+    
+    public boolean proceso_entrada(int entrada) {
+        if (entrada == this.incognita) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    
+}
