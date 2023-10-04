7873cc2cfffa4b505ed6dfb6c165049bd5b5f0b0
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2023-10-04 11:37:25 -0300

    Uso de interface, ejemplo de interface Comparable

---
+++ demo_interface/CuentaBanco.java
@@ -0,0 +1,37 @@
+package demo_interface;
+
+public class CuentaBanco implements Comparable<CuentaBanco> {
+    // El parámetro es de tipo de la misma clase (CuentaBanco)
+    
+    private Double balance;
+
+    public CuentaBanco(double balance) {
+        this.balance = balance;
+    }
+
+    public CuentaBanco() {
+    }
+
+    public int compareTo(CuentaBanco other) {
+        if (balance < other.getSaldo()) {
+            return -1;
+        }
+        if (balance > other.getSaldo()) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public double getSaldo() {
+        return balance;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
